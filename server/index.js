a6b36577b7ac0f3c58a537bf121f1ec2dd5c880d
jackzhen1996 <jackzhen999@gmail.com>
2021-05-02 16:10:23 -0700

    removed stuff

--- server/index.js
+++ server/index.js
@@ -8,15 +8,7 @@ const path = require('path');
 const io = require("socket.io")(http,{
   cors: {
     origin: ['*'],
-    handlePreflightRequest:  (req,res) => {
-      res.writeHead(200, {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST',
-        'Access-Control-Allow-Headers': 'my-custom-header',
-        'Access-Control-Allow-Credentials': true
-      });
-      res.end();
-    }
+    methods: ['GET','POST']
   }
 });
 require('dotenv').config();
